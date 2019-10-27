c76b6178fbd25331c097785276d95fa26186905f
David Turner <csusbdt@gmail.com>
2019-10-26 20:45:12 -0700

    X

--- a.js
+++ a.js
@@ -1,28 +1,4 @@
 window.addEventListener('load', function() {
   window.a = window.a || {};
-  const clockCtx = a_clock.getContext('2d');
-  let   t        = 0;
-
-  function clock(millis) {
-    const dt = millis / 1000 - t;
-    t        = millis / 1000;
-    clockCtx.clearRect(0, 0, a_clock.width, a_clock.height);
-    const now = new Date();
-    let ampm = "AM";
-    let hour = now.getHours();
-    if (hour > 12) { 
-      hour -= 12; 
-      ampm = "PM"; 
-    }
-    let minute = now.getMinutes();
-    if (minute < 10) minute = "0" + minute; 
-    let s = hour + ":" + minute + " " + ampm;
-    a.font.draw(clockCtx, s, 0, 0);
-    requestAnimationFrame(clock);
-  }
-  requestAnimationFrame(function(millis) {
-    t = millis / 1000;
-    requestAnimationFrame(clock);
-  });
 });
 
---
+++ clock.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', function() {
+  const clockCtx = a_clock.getContext('2d');
+  let   t        = 0;
+
+  function clock(millis) {
+    const dt = millis / 1000 - t;
+    t        = millis / 1000;
+    clockCtx.clearRect(0, 0, a_clock.width, a_clock.height);
+    const now = new Date();
+    let ampm = "AM";
+    let hour = now.getHours();
+    if (hour > 12) { 
+      hour -= 12; 
+      ampm = "PM"; 
+    }
+    let minute = now.getMinutes();
+    if (minute < 10) minute = "0" + minute; 
+    let s = hour + ":" + minute + " " + ampm;
+    a.font.draw(clockCtx, s, 0, 0);
+    requestAnimationFrame(clock);
+  }
+  requestAnimationFrame(function(millis) {
+    t = millis / 1000;
+    requestAnimationFrame(clock);
+  });
+});
+
